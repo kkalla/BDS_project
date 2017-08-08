945d8351062adf5565fa0275524dd2a7ac6dd04b
kkalla <dkswns333@gmail.com>
2017-08-08 15:18:15 +0900

    Update data

---
+++ get_longlat.R
@@ -0,0 +1,50 @@
+library(httr)
+library(jsonlite)
+files <- dir('Data/경기도_공유재산')
+data_list <- list()
+
+clientID <- read.csv('naver_api.txt',header=FALSE,stringsAsFactor=FALSE)[2,1]
+clientSecret <-read.csv('naver_api.txt',header=FALSE,
+                        stringsAsFactor=FALSE)[2,2]
+
+for(i in 1:32){
+    data_list[[i]] <- read.csv(paste('Data/경기도_공유재산/',files[i],sep=""))
+    df <- data_list[[i]]
+    df$address <- paste(df$시군구명,df$읍면동명,df$리명,df$본번,"-",df$부번)
+    data_list[[i]] <- df
+}
+
+get_geocode <- function(adr){
+    ## adr is vector of addresses
+    n <- length(adr)
+    api <- 'https://openapi.naver.com/v1/map/geocode'
+    lonlat <- data.frame()
+    for(i in 1:n){
+        adr_i <- adr[i]
+        adr_i <- gsub(" - 0","",adr_i)
+        adr_i <- URLencode(adr_i)
+        request_url <- paste(api,"?query=",adr_i,sep="")
+        geo_json <- GET(request_url,
+                        add_headers('X-Naver-Client-Id'=clientID,
+                                    'X-Naver-Client-Secret'=clientSecret))
+        if(geo_json$status_code == 200){
+            geo <- fromJSON(toJSON(content(geo_json)))
+            lonlat <- rbind(lonlat,geo$result$items$point)
+        } else {
+            ## If error occurs
+            lonlat <- rbind(lonlat,c(x=999,y=999))
+        }
+    }
+    return(lonlat)
+}
+
+for(i in 1:length(data_list)){
+    lonlat <- get_geocode(data_list[[i]]$address)
+    data_list[[i]]$longitude <- lonlat[,1]
+    data_list[[i]]$latitude <- lonlat[,2]
+}
+
+for(i in 1:length(data_list)){
+    write.csv(paste('Data/경기도_공유재산/',files[i],sep=""))
+}
+lonlat <- get_geocode(data_list[[1]]$address)
---
+++ splitting_data.R
@@ -0,0 +1,18 @@
+data <- read.csv('Data/공유재산현황/공유재산현황.csv',stringsAsFactors=FALSE)
+str(data)
+
+## 시별로 set 나누기
+sigungu <- unique(strtrim(data$시군구명,13))
+?strtrim
+sigungu2 <- sigungu[strtrim(sigungu,6)=='경기도']
+data_splited <- list(data.frame())
+for (i in 1:length(sigungu2)){
+    data_splited[[i]] <- data[strtrim(data$시군구명,13) %in% sigungu2[i],]
+}
+
+## Save data frame as csv
+for(i in 1:length(sigungu2)){
+    write.csv(data_splited[[i]],
+              file=paste("Data/경기도 공유재산(시군구별)/",
+                         sigungu2[i],".csv",sep=""))
+}
